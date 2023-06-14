f9e81a8cd841cc9f7fc0e07b734e779b443e8a4d
Reguluz <154496676@qq.com>
2023-06-15 00:34:32 +0800

    Add Define Symbol / Remove Define Symbol Function

---
+++ Editor/MFRefLinkDefSetting.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+
+namespace Tools.Editor.MFAssetTools.MFRefLink.Editor
+{
+    public class MFRefLinkDefSetting
+    {
+        private const string DefineSymbol = "MFRefLink";
+        
+        [MenuItem("Moonflow/Utility/RefLink/Add Define Symbol")]
+        public static void AddDefineSymbol()
+        {
+            //add define symbol to unity project
+            var symbols = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
+            if (symbols.Contains(DefineSymbol))
+            {
+                return;
+            }
+            symbols += ";" + DefineSymbol;
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup, symbols);
+        }
+        
+        [MenuItem("Moonflow/Utility/RefLink/Remove Define Symbol")]
+        public static void RemoveDefineSymbol()
+        {
+            //remove define symbol to unity project
+            var symbols = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
+            if (!symbols.Contains(DefineSymbol))
+            {
+                return;
+            }
+            symbols = symbols.Replace(DefineSymbol, "");
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup, symbols);
+        }
+    }
+}
