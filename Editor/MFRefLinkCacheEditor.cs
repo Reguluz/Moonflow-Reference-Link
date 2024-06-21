46ca6f48e249ebf436506a6a4941556c51e4aed1
frankmhli <frankmhli@tencent.com>
2024-06-21 20:16:26 +0800

    RefCacheEditor

---
+++ Editor/MFRefLinkCacheEditor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Moonflow;
+using Moonflow.MFAssetTools.MFRefLink;
+using UnityEditor;
+
+namespace Tools.Editor.MFAssetTools.MFRefLink.Editor
+{
+    [CustomEditor(typeof(MFRefLinkCache))]
+    public class MFRefLinkCacheEditor : UnityEditor.Editor
+    {
+        private MFRefLinkCache _target;
+        private int index;
+        private void OnEnable()
+        {
+            _target = target as MFRefLinkCache;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            MFEditorUI.DrawFlipList(_target.refLinkDict,ref index, 50);
+        }
+    }
+}
